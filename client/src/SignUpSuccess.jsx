10a5207f651f9768a07add5685c7fab4e1853e87
Emmy Manning <emswoo@EM.local>
2024-03-10 20:50:52 -0400

    updated layout and style

--- client/src/SignUpSuccess.jsx
+++ client/src/SignUpSuccess.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const SignUpSuccess = () => {
   return (
-    <div>SignUpSuccess</div>
-  )
-}
+    <div className='flex justify-center items-center bg-[#202020] w-full h-screen'>
+      <p className='text-white text-xl'>
+        Thank you. Please login{' '}
+        <a href='/' className='text-[#53D3D4]'>
+          here
+        </a>
+        .
+      </p>
+    </div>
+  );
+};
 
-export default SignUpSuccess
+export default SignUpSuccess;
