67a97262578503544f0d21942371081c2378373e
Emmy Manning <emswoo@EM.local>
2024-03-10 20:30:58 -0400

    hero section removed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginForm from './LoginForm';
 import SignUpForm from './SignUpForm';
 import SignUpSuccess from './SignUpSuccess';
-import HeroSection from './HeroSection';
 
 const App = () => {
   return (
@@ -11,7 +10,6 @@ const App = () => {
         <Route path='/' element={<LoginForm />} />
         <Route path='/sign-up' element={<SignUpForm />} />
         <Route path='/success' element={<SignUpSuccess />} />
-        <Route path='/home' element={<HeroSection />} />
       </Routes>
     </Router>
   );
--- client/src/HeroSection.jsx
+++
@@ -1,9 +0,0 @@
-const HeroSection = () => {
-  return (
-    <div>
-      <h1 className='text-4xl font-bold'>You're Logged In!</h1>
-    </div>
-  );
-};
-
-export default HeroSection;
