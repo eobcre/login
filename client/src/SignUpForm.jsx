958222989b143f38c88d91d44d4ec67fe883590a
Emmy Manning <emswoo@EM.local>
2024-03-10 20:50:33 -0400

    updated validation

--- client/src/SignUpForm.jsx
+++ client/src/SignUpForm.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 const SignUpForm = () => {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({ username: '', password: '' });
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: 'onChange' });
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = async (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
 
     try {
@@ -32,7 +39,7 @@ const SignUpForm = () => {
   return (
     <div className='flex justify-center items-center bg-[#202020] w-full h-screen'>
       <form
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmit(onSubmit)}
         className='flex flex-col border border-[#53D3D4] rounded-md p-10 w-[480px]'
       >
         <label htmlFor='username' className='text-white'>
@@ -46,7 +53,11 @@ const SignUpForm = () => {
           onChange={handleChange}
           placeholder='Enter your user name'
           className='rounded-md outline-none mt-1 mb-2 p-3'
+          {...register('name', {
+            required: '* User Name is required.',
+          })}
         />
+        <p className='text-red-500 mb-3'>{errors.name?.message}</p>
 
         <label htmlFor='password' className='text-white'>
           Password
@@ -59,12 +70,20 @@ const SignUpForm = () => {
           onChange={handleChange}
           placeholder='Enter your password'
           className='rounded-md outline-none mt-1 mb-2 p-3'
+          {...register('password', {
+            required: '* Password is required.',
+            minLength: {
+              value: 8,
+              message: '* Must contain 8 or more characters.',
+            },
+          })}
         />
+        <p className='text-red-500 mb-4'>{errors.password?.message}</p>
 
         <div className='flex'>
           <input
             type='submit'
-            value='Register'
+            value='Sign Up'
             className='text-white border border-[#53D3D4] rounded-md m-auto mt-6 p-2 w-[100px]'
           />
         </div>
